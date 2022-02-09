ea6e77c1e9cc64abed90eea3875a60d19bce238b
rtSoftware <tapiara@yahoo.com>
2022-02-09 07:27:00 -0500

    primera actualizacion

---
+++ src/pages/hola.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+//import Seo from "../components/seo"
+
+const Hola = () => (
+  <Layout>
+    <h1>Hi from hola page</h1>
+    <p>Welcome to page 2</p>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default Hola
