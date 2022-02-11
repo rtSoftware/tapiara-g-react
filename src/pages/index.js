1c3c571ffe0e0d3ed8851e64df26a80f9595f56a
rtSoftware <tapiara@yahoo.com>
2022-02-11 15:27:51 -0500

    upload automatico netlify...

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hola amigños !</h1>
+    <h1>Hola amigos mios...</h1>
     <p>Bienvenidos a mi sitio.</p>
     s<StaticImage
       src="../images/webTapiara.jpg"
